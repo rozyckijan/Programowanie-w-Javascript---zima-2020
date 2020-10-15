1373a71497f2e70ac8f5cb9346aa9535503fbbed
Rysiek Brzegowy <r.brzegowy@gmail.com>
2020-10-15 14:58:17 +0200

    inf3 cw2 drumkit eod

---
+++ inf3/cw2 - drumkit/main.js
@@ -0,0 +1,55 @@
+document.body.addEventListener('keypress', onKeyPress);
+document.querySelector('#recordBtn').addEventListener('click', onRecordBtnClick);
+document.querySelector('#playBtn').addEventListener('click', onPlayBtnClick);
+
+let recordStartTime;
+const recordedSounds = [];
+
+function onKeyPress(ev) {
+    let soundName;
+    switch (ev.code) {
+    case 'KeyA':
+        soundName = 'boom';
+        break;
+    case 'KeyS':
+        soundName = 'clap';
+        break;
+    case 'KeyD':
+        soundName = 'hihat';
+        break;
+    case 'KeyF':
+        soundName = 'kick';
+        break;
+    }
+    if (soundName) {
+        const soundTime = Date.now() - recordStartTime;
+        const soundObj = {
+            id: soundName, 
+            time: soundTime
+        };
+        recordedSounds.push(soundObj);
+        playSound(soundName);
+    }
+}
+
+function onRecordBtnClick() {
+    recordStartTime = Date.now();
+}
+function onPlayBtnClick() {
+    for (let index = 0; index < recordedSounds.length; index++) {
+        const soundObj = recordedSounds[index];
+        setTimeout(
+            () => {
+                playSound(soundObj.id);
+            },
+            soundObj.time
+        );
+        
+    }
+}
+
+function playSound(id) {
+    const sound = document.querySelector('#' + id);
+    sound.currentTime = 0;
+    sound.play();
+}
