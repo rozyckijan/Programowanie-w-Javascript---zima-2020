50d57792753e586f4b8bdabf741e87ee6685b876
Rysiek Brzegowy <r.brzegowy@gmail.com>
2020-11-05 16:00:22 +0100

    inf1 class refactor

---
+++ inf1/cw3 - notekeep/klasy-notatnik-z-zajec.js
@@ -0,0 +1,99 @@
+// ---------------------------
+// only scratch from labs not completed/tested code!
+// --------------------------
+
+// note.js
+class Note {
+    constructor(title, content, color = 'red', pinned = false) {        
+        this.title  = title;
+        this.content = content;
+        this.color = color;
+        this.pinned = pinned;
+        this.createDate = new Date();
+        this.id = Date.now();
+    }    
+}
+
+// notes.js
+class Notes {
+    constructor(containerSelector) {
+        this.notesArr = [];
+        this.db = new Db();
+        this.notesUI = new NotesUI(containerSelector);
+    }
+
+    addNote(note) {
+        this.notesArr.push(note);
+        this.db.saveNotes(this.notesArr);
+        this.notesUI.addNote(note);
+    }
+    removeNote(id) {
+        this.notesArr = this.notesArr.filter(el => el.id !== id);
+        // this.notesArr.findeIndex, then this.notesArr.splice        
+        this.db.saveNotes(this.notesArr);
+        this.notesUI.removeNote(id);
+    }
+
+    getNote(id) {
+        return this.notesArr.find(el => el.id === id);
+    }
+
+    getNotes() {
+        return [...this.notesArr];
+    }
+}
+
+// db.js
+class Db {
+    constructor() {
+        this.lsNotesKey = 'notes';
+    }
+    saveNotes(notes) {
+        localStorage.setItem(this.lsNotesKey, JSON.stringify(notes));
+    }
+    getNotes() {
+        // check if localStorage has item this.lsNotesKey
+        return JSON.parse(localStorage.getItem(this.lsNotesKey));
+    }
+}
+
+// notes-ui.js
+class NotesUI {
+    constructor(containerSelector = 'section') {
+        this.notesContainer = document.querySelector(containerSelector);
+    }
+    addNote(note) {
+        const htmlNote = this.createNote(note);
+        const container = this.getNotesContainer();
+        container.appendChild(htmlNote);
+    }
+    createNote(note) {
+        const htmlNote = document.createElement('div');
+        htmlNote.classList.add('note');
+        // do all the things with notehtml object - create title, content ...
+        return htmlNote;
+    }
+    removeNote(id) {
+        const note = this.getNote(id);
+        const container = this.getNotesContainer();
+        container.removeChild(note);
+    }
+    getNote(id) {
+        return document.querySelector('#' + id);
+    }
+    getNotesContainer() {
+        return this.notesContainer;
+    }
+}
+
+// main.js
+const notesObj = new Notes();
+
+document.querySelector('#addNewNote').addEventListener('click', onNewNote);
+
+
+// const notesArr = notesObj.getNotes()
+
+// notes collection
+// storage 
+// ui operations 
